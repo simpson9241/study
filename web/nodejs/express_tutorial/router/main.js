5ebe46c2d37ed5a853bd8d784e247966f53522b2
simpson9241 <simpson9241@gmail.com>
2020-08-10 18:19:57 +0900

    commit_done

--- web/nodejs/express_tutorial/router/main.js
+++ web/nodejs/express_tutorial/router/main.js
@@ -1,11 +1,88 @@
 module.exports=function(app,fs)
 {
   app.get('/',function(req,res){
+    var sess=req.session;
+
     res.render('index',{
       title:"MY HOMEPAGE",
-      length: 5
+      length: 5,
+      name:sess.name,
+      username:sess.username
     })
   });
+
+  //세션 초기 설정
+  app.get('/',function(req,res){
+    sess=req.session;
+  });
+
+  //세션 변수 설정
+  app.get('/login',function(req,res){
+    sess=req.session;
+    sess.username="velopert";
+  });
+
+  //세션 변수 사용
+  app.get('/',function(req,res){
+    sess=req.session;
+    console.log(sess.username);
+  });
+
+  //세션 제거
+  app.get('/',function(req,res){
+    req.session.destroy(function(err){
+    // cannot access session here
+    })
+  });
+
+
+  //로그인 API
+  app.get('/login/:username/:password',function(req,res){
+    var sess;
+    sess=req.session;
+
+    fs.readFile(__dirname+"/../data/user.json","utf8",function(err,data){
+      var users=JSON.parse(data);
+      var username=req.params.username;
+      var password=req.params.password;
+      var result={};
+      if(!users[username]){
+        //Username Not Found
+        result["success"]=0;
+        result["error"]="not found";
+        res.json(result);
+        return;
+      }
+
+      if(users[username]["password"]==password){
+        result["success"]=1;
+        sess.username=username;
+        sess.name=users[username]["name"];
+        res.json(result);
+      }else{
+        result["success"]=0;
+        result["error"]="incorrect";
+        res.json(result);
+      }
+    })
+  });
+
+  //로그아웃 API
+  app.get('/logout',function(req,res){
+    sess=req.session;
+    if(sess.username){
+      req.session.destroy(function(err){
+        if(err){
+          console.log(err);
+        }else{
+          res.redirect('/');
+        }
+      })
+    }else{
+      res.redirect('/');
+    }
+  });
+
 
   app.get('/list',function(req,res){
     fs.readFile(__dirname+"/../data/"+"user.json",'utf8',function(err,data){
@@ -103,7 +180,7 @@ module.exports=function(app,fs)
                     return;
                   })
     })
-  })
+  });
 
 
 
