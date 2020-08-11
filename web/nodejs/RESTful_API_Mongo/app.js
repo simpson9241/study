cc76f3c1e1edbe9098c24183b3b6f6460adc6fb3
simpson9241 <simpson9241@gmail.com>
2020-08-11 15:00:41 +0900

    commit

--- web/nodejs/RESTful_API_Mongo/app.js
+++ web/nodejs/RESTful_API_Mongo/app.js
@@ -11,12 +11,28 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 //[Configure Server Port]
+//process.env.PORT 는 리눅스 터미널에서 export PORT [포트 번호] 로 변수 설정해줘야된다.
 var port=process.env.PORT || 8080;
 
-//[Configure Router]
-var router=require('./routes')(app)
+
 
 //[Run Server]
 var server = app.listen(port, function(){
   console.log("Express server has started on port "+port);
 })
+
+//[Configure to MongoDB Server]
+var db=mongoose.connection;
+db.on('error',console.error);
+db.once('open',function(){
+  console.log("Connected to mongod server");
+});
+
+//Connect to MongoDB Server
+mongoose.connect('mongodb://localhost/mongodb_tutorial')
+
+//Define model
+var Book=require('./models/book');
+
+//[Configure Router]
+var router=require('./routes')(app,Book);
--- web/nodejs/RESTful_API_Mongo/routes/index.js
+++ web/nodejs/RESTful_API_Mongo/routes/index.js
@@ -1,21 +1,77 @@
 //routes/index.js
 
-module.exports=function(app)
+module.exports=function(app,Book)
 {
   //Get All Books
   app.get('/api/books',function(req,res){
-    res.end();
+    Book.find(function(err,books){
+      if(err) return res.status(500).send({error:'database failure'});
+      res.json(books);
+    })
   });
 
   //Get Single Book
   app.get('/api/books/:book_id',function(req,res){
-    res.end();
+    Book.findOne({_id:req.params.book_id},function(err,book){
+      if(err)return res.status(500).json({error:err});
+      if(!book) return res.status(404).json({error:'book not found'});
+      res.json(book);
+    })
   });
 
   //Get Book by Author
   app.get('/api/books/author/:author',function(req,res){
-    res.end();
+    Book.find({author:req.params.author},{_id:0,title:1,published_date:1},function(err,books){
+      if(err)return res.status(500).json({error:err});
+      if(books.length===0)return res.status(404).json({error:'book not found'});
+      res.json(books);
+    })
   });
 
-  
+  //Create Book
+  app.post('/api/books',function(req,res){
+    var book=new Book();
+    book.title=req.body.title;
+    book.author=req.body.author;
+    book.published_date=new Date(req.body.published_date);
+
+    book.save(function(err){
+      if(err){
+        console.error(err);
+        res.json({result:0});
+        return;
+      }
+
+      res.json({result:1});
+    });
+  });
+
+  //Update the Book
+  app.put('/api/books/:book_id',function(req,res){
+    Book.findById(req.params.book_id, function(err,book){
+      if(err)return res.status(500).json({error:'database failure'});
+      if(!book)return res.status(404).json({error:'book not found'});
+      if(req.body.title)book.title=req.body.title;
+      if(req.body.title)book.author=req.body.author;
+      if(req.body.published_date)book.published_date=req.body.published_date;
+
+      book.save(function (err){
+        if(err) res.status(500).json({error:'failed to update'});
+        res.json({message: 'book updated'});
+      });
+    });
+  });
+
+  //Delete Book
+  app.delete('/api/books/:book_id',function(req,res){
+    Book.remove({_id:req.params.book_id},function(err,output){
+      if(err) return res.status(500).json({error:"database failure"});
+
+      /*Since Delete Operation is Idempotent, no need to specify*/
+      //if(!output.result.n) return res.status(404).json({error: "book not found"});
+      //res.json({message: "book deleted"});
+      res.status(204).end();
+    })
+  });
+
 }
