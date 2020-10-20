5f8f02f79f25452117cc3c04aa8b48fdd8baa080
simpson9241 <simpson9241@gmail.com>
2020-10-20 15:14:54 +0900

    commit

--- web/onlinemp4parser/main.js
+++ web/onlinemp4parser/main.js
@@ -19,6 +19,9 @@ function previewVideo(f){
   reader.onload=function(rst){
     document.getElementById('video_show').innerHTML='<video autoplay controls loop muted preload="none">'+'<source src="'+rst.target.result+'" type="video/mp4">'+'Error'+'</video>';
     // console.log(rst.target.result);
+    let base64result=rst.target.result.split(',')[1];
+    let decodedresult=atob(base64result);
+    console.log(decodedresult);
   }
 
   reader.readAsDataURL(file[0]);
